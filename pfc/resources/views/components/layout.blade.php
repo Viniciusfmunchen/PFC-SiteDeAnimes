7503853c2fd298bb9246b4c6c6833b1bf9017a61
Vinicius Fontana Munchen <viniciusfm.acad@gmail.com>
2022-09-14 14:03:29 -0300

    Criação projeto

---
+++ pfc/resources/views/components/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}} - Soka Animes</title>
+    @vite(['resources/js/app.js'])
+
+<body>
+<div class="container">
+    <h1>{{$title}}</h1>
+
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    {{$slot}}
+
+</div>
+</body>
+
+</html>
---
+++ pfc/resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<x-layout title="Home">
+    <h1>Ola mundo!</h1>
+</x-layout>
+
